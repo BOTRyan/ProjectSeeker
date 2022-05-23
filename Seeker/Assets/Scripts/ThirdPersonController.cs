de86cb7534c844cdb5fdf351f367ebc3c4cb7a7d
David <davidvardgas@gmail.com>
2022-05-23 17:50:06 -0400

    Initial Movement
    -Prefabs for both Seeker and Hider made
    -Scan function for Seeker started

--- Seeker/Assets/Scripts/ThirdPersonController.cs
+++ Seeker/Assets/Scripts/ThirdPersonController.cs
@@ -175,6 +175,7 @@ namespace StarterAssets
 
 		private void Move()
 		{
+			
 			// set target speed based on move speed, sprint speed and if sprint is pressed
 			float targetSpeed = _input.sprint ? SprintSpeed : MoveSpeed;
 
